5db19807fe30bce29b479c2961d42831b2cd6931
Carson <zmcddn@hotmail.com>
2018-11-09 00:04:24 -0700

    Initial hist plot file

---
+++ dshelper/plot/hist.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+from numpy import arange, sin, pi
+
+import matplotlib
+import seaborn as sns
+
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg as FigureCanvas
+from matplotlib.backends.backend_wx import NavigationToolbar2Wx
+from matplotlib.figure import Figure
