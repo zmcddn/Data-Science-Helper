caed2232a63e5add6d907b169ec749f9adf2926c
Carson <zmcddn@hotmail.com>
2018-11-16 23:28:11 -0700

    Add distribution plot place holder

--- dshelper/plot/plot_panel.py
+++ dshelper/plot/plot_panel.py
@@ -35,6 +35,7 @@ class PlotPanel(wx.Panel):
         plot_notebook.SetBackgroundColour("WHITE")
         self.hist_page = HistPanel(plot_notebook, df=self.df)
         self.heat_page = HeatPanel(plot_notebook, df=self.df)
+        self.distribution_page = wx.Panel(plot_notebook)
         self.scatter_page = wx.Panel(plot_notebook)
         self.boxplot_page = wx.Panel(plot_notebook)
         self.violin_page = wx.Panel(plot_notebook)
@@ -42,6 +43,7 @@ class PlotPanel(wx.Panel):
         # Add pages into the notebook for display
         plot_notebook.AddPage(self.hist_page, "Histogram")
         plot_notebook.AddPage(self.heat_page, "Heat Map")
+        plot_notebook.AddPage(self.distribution_page, "Distribution")
         plot_notebook.AddPage(self.scatter_page, "Scatter Plot")
         plot_notebook.AddPage(self.boxplot_page, "Box Plot")
         plot_notebook.AddPage(self.violin_page, "Violin Plot")
