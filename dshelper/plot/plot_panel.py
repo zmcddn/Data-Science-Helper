1710559e3cbadb3f1faf25be902baa775189f347
Carson <zmcddn@hotmail.com>
2018-12-06 22:49:35 -0700

    Get rid of distribution plot

--- dshelper/plot/plot_panel.py
+++ dshelper/plot/plot_panel.py
@@ -35,20 +35,18 @@ class PlotPanel(wx.Panel):
         plot_notebook.SetBackgroundColour("WHITE")
         self.hist_page = HistPanel(plot_notebook, df=self.df)
         self.heat_page = HeatPanel(plot_notebook, df=self.df)
-        self.distribution_page = wx.Panel(plot_notebook)
+        # self.distribution_page = wx.Panel(plot_notebook)
         self.scatter_page = ScatterPanel(plot_notebook, df=self.df)
         self.box_violin_page = BoxViolinPanel(plot_notebook, df=self.df)
         self.pair_page = PairPanel(plot_notebook, df=self.df)
-        # self.violin_page = wx.Panel(plot_notebook)
 
         # Add pages into the notebook for display
         plot_notebook.AddPage(self.hist_page, "Histogram")
         plot_notebook.AddPage(self.heat_page, "Heat Map")
-        plot_notebook.AddPage(self.distribution_page, "Distribution")
+        # plot_notebook.AddPage(self.distribution_page, "Distribution")
         plot_notebook.AddPage(self.scatter_page, "Scatter Plot")
         plot_notebook.AddPage(self.box_violin_page, "Box and Violin Plots")
         plot_notebook.AddPage(self.pair_page, "Pair Plots")
-        # plot_notebook.AddPage(self.violin_page, "Violin Plot")
 
         # Put the notebook in a sizer in the panel for layout
         sizer = wx.BoxSizer()
