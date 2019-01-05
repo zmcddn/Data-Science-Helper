87279e5c6fc21ac747033b59be09617871903e44
Carson <zmcddn@hotmail.com>
2019-01-05 01:04:22 -0700

    Move the data cleaning out of the pairplots into a utility function

--- dshelper/plot/pair.py
+++ dshelper/plot/pair.py
@@ -26,6 +26,8 @@ from matplotlib.backends.backend_wx import NavigationToolbar2Wx as NavigationToo
 from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
 
+from .utils import prepare_data
+
 
 class PairPanel(wx.Panel):
     """
@@ -82,73 +84,11 @@ class PairPanel(wx.Panel):
         # Reset plot, clean the axes
         self.figure.clf()
 
-        start_message = "\nPrepare to plot pair plots ..."
-        pub.sendMessage("LOG_MESSAGE", log_message=start_message)
-        _spacing = " " * 7
+        df = prepare_data(self.df[self.available_columns])
 
-        df = self.df[self.available_columns]
-        label = LabelEncoder()
-
-        encoding_drop_columns = []
-        for num, column_type in enumerate(df.dtypes):
-            original_column_name = df.columns[num]
-            _message = "--> Processing column: {} --> {}".format(
-                original_column_name, column_type
-            )
-            pub.sendMessage("LOG_MESSAGE", log_message=_message)
-
-            if str(column_type) == "object":
-                try:
-                    # Case for datetime data
-                    df["new_datetime_column"] = pd.to_datetime(df[original_column_name])
-
-                    # Plot the datetime for pairplot as categorical data for now
-                    df.drop("new_datetime_column", axis=1, inplace=True)
-                except ValueError:
-                    # Case for categorical data
-
-                    if df[original_column_name].isnull().values.any():
-                        # Fill categorical missing values with mode
-                        df[original_column_name].fillna(
-                            df[original_column_name].mode()[0], inplace=True
-                        )
-
-                    pub.sendMessage(
-                        "LOG_MESSAGE", log_message="{}Encoding...".format(_spacing)
-                    )
-
-                    try:
-                        # Clean categorical data
-                        new_column_name = original_column_name + "_code"
-                        df[new_column_name] = label.fit_transform(
-                            df[original_column_name]
-                        )
-                        encoding_drop_columns.append(original_column_name)
-
-                        if column_name == original_column_name:
-                            # Update hue column
-                            column_name = new_column_name
-
-                    except (ValueError, TypeError) as e:
-                        encoding_drop_columns.append(original_column_name)
-                        _message = "{}Column [{}] droped <--".format(
-                            _spacing, original_column_name
-                        )
-                        pub.sendMessage("LOG_MESSAGE", log_message=_message)
-
-                    pub.sendMessage(
-                        "LOG_MESSAGE", log_message="{}Finished".format(_spacing)
-                    )
-            else:
-                if df[original_column_name].isnull().values.any():
-                    # Fill numerical missing values with median
-                    df[original_column_name].fillna(
-                        df[original_column_name].median(), inplace=True
-                    )
-
-        # Drop all the original categorical data columns
-        if encoding_drop_columns:
-            df.drop(encoding_drop_columns, axis=1, inplace=True)
+        if str(self.df[column_name].dtype) == "object":
+            # Update hue column for categorical data
+            column_name += "_code"
 
         pub.sendMessage("LOG_MESSAGE", log_message="\nReady to plot...")
 
---
+++ dshelper/plot/utils.py
@@ -0,0 +1,88 @@
+import pandas as pd
+import numpy as np
+from numpy import arange, sin, pi
+
+from wx.lib.pubsub import pub
+
+from sklearn.preprocessing import LabelEncoder
+
+
+def prepare_data(df):
+    """
+    A helper function to prepare the data for plot.
+
+    The way it cleans follows the standard data science way of cleaning:
+        categorical data: 
+            encode with LabelEncoder
+            fillna with mode
+        numerical data:
+            fillna with median
+
+    input: raw data frame
+    output: data frame with encoded categorical data and without any null data
+    """
+    
+    label = LabelEncoder()
+
+    start_message = "\nPrepare to plot pair plots ..."
+    pub.sendMessage("LOG_MESSAGE", log_message=start_message)
+    _spacing = " " * 7
+
+    encoding_drop_columns = []
+    for num, column_type in enumerate(df.dtypes):
+        original_column_name = df.columns[num]
+        _message = "--> Processing column: {} --> {}".format(
+            original_column_name, column_type
+        )
+        pub.sendMessage("LOG_MESSAGE", log_message=_message)
+
+        if str(column_type) == "object":
+            try:
+                # Case for datetime data
+                df["new_datetime_column"] = pd.to_datetime(df[original_column_name])
+
+                # Plot the datetime for pairplot as categorical data for now
+                df.drop("new_datetime_column", axis=1, inplace=True)
+            except ValueError:
+                # Case for categorical data
+
+                if df[original_column_name].isnull().values.any():
+                    # Fill categorical missing values with mode
+                    df[original_column_name].fillna(
+                        df[original_column_name].mode()[0], inplace=True
+                    )
+
+                pub.sendMessage(
+                    "LOG_MESSAGE", log_message="{}Encoding...".format(_spacing)
+                )
+
+                try:
+                    # Clean categorical data
+                    new_column_name = original_column_name + "_code"
+                    df[new_column_name] = label.fit_transform(
+                        df[original_column_name]
+                    )
+                    encoding_drop_columns.append(original_column_name)
+
+                except (ValueError, TypeError) as e:
+                    encoding_drop_columns.append(original_column_name)
+                    _message = "{}Column [{}] droped <--".format(
+                        _spacing, original_column_name
+                    )
+                    pub.sendMessage("LOG_MESSAGE", log_message=_message)
+
+                pub.sendMessage(
+                    "LOG_MESSAGE", log_message="{}Finished".format(_spacing)
+                )
+        else:
+            if df[original_column_name].isnull().values.any():
+                # Fill numerical missing values with median
+                df[original_column_name].fillna(
+                    df[original_column_name].median(), inplace=True
+                )
+
+    # Drop all the original categorical data columns
+    if encoding_drop_columns:
+        df.drop(encoding_drop_columns, axis=1, inplace=True)
+
+    return df
