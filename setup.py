4d61aa5c92a358b2e06172af4fd9ddcbcf87ee54
Carson <zmcddn@hotmail.com>
2019-01-10 00:50:03 -0700

    Add setup file

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='dshelper',
+    version='0.1.0',
+    description='A GUI tool to display pandas dataframe and draw plots in a few clicks',
+    url='https://github.com/zmcddn/Data-Science-Helper',
+    author='Minchang (Carson) Zhang',
+    author_email='minchang@ualberta.ca',
+    license='MIT',
+    packages=['dshelper'],
+    install_requires=[
+        'wxpython>=3.0',
+        'matplotlib>=2.1.2',
+        'numpy>=1.14.0',
+        'scipy>=1.0.0',
+        'pandas>=0.22.0',
+        'scikit-learn>=0.19.1',
+        'seaborn>=0.8.0'
+    ],
+    zip_safe=False
+)
