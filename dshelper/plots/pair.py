77a14cf1c7fb167485dac4048ace91fcfed01ff9
Carson <zmcddn@hotmail.com>
2021-01-06 23:21:00 -0700

    Extract the pair plot func

--- dshelper/plots/pair.py
+++ dshelper/plots/pair.py
@@ -19,7 +19,7 @@ from matplotlib.backends.backend_wx import NavigationToolbar2Wx as NavigationToo
 from matplotlib.figure import Figure
 
 from components import create_bitmap_dropdown_menu
-from .utils import prepare_data
+from .utils import make_pair_plot
 
 
 class PairPanel(wx.Panel):
@@ -36,6 +36,7 @@ class PairPanel(wx.Panel):
         wx.Panel.__init__(self, parent)
 
         self.df = df
+
         self.available_columns = list(self.df.columns)
         self.hue_columns = self._get_hue_column()
 
@@ -91,129 +92,7 @@ class PairPanel(wx.Panel):
         Returns: None
         """
 
-        # Reset plot, clean the axes
-        self.figure.clf()
-
-        legend_labels = self.df[column_name].unique()
-        legend_title = column_name
-
-        df = prepare_data(self.df[self.available_columns])
-
-        if str(self.df[column_name].dtype) == "object":
-            # Update hue column for categorical data
-            column_name += "_code"
-
-        pub.sendMessage("LOG_MESSAGE", log_message="\nReady to plot...")
-
-        try:
-            # Produce pairplot using seaborn
-            pair_plot = sns.pairplot(
-                df,
-                hue=column_name,
-                palette="deep",
-                size=1.2,
-                diag_kind="kde",
-                diag_kws=dict(shade=True),
-                plot_kws=dict(s=10),
-            )
-
-            # Get the number of rows and columns from the seaborn pairplot
-            pp_rows = len(pair_plot.axes)
-            pp_cols = len(pair_plot.axes[0])
-
-            # Update axes to the corresponding number of subplots from pairplot
-            self.axes = self.figure.subplots(pp_rows, pp_cols)
-
-            # Get the label and plotting order
-            x_labels, y_labels = [], []
-            for ax in pair_plot.axes[-1, :]:
-                xlabel = ax.xaxis.get_label_text()
-                x_labels.append(xlabel)
-            for ax in pair_plot.axes[:, 0]:
-                ylabel = ax.yaxis.get_label_text()
-                y_labels.append(ylabel)
-
-            # Setup hue for plots
-            hue_values = df[column_name].unique()
-            palette = sns.color_palette("muted") # get seaborn default colors
-            legend_color = palette.as_hex()
-
-            # Mimic how seaborn produce the pairplot using matplotlib subplots
-            for i in range(len(x_labels)):
-                for j in range(len(y_labels)):
-                    if i != j:
-                        # Non-diagonal locations, scatter plot
-                        for num, value in enumerate(hue_values):
-                            sns.regplot(
-                                x=df[x_labels[i]][df[column_name] == value],
-                                y=df[y_labels[j]][df[column_name] == value],
-                                data=df,
-                                scatter=True,
-                                fit_reg=False,
-                                ax=self.axes[j, i],
-                                scatter_kws={
-                                    's':10, # Set dot size
-                                    'facecolor':legend_color[num], # Set dot color
-                                }
-                            )
-                    else:
-                        # Diagonal locations, distribution plot
-                        for num, value in enumerate(hue_values):
-                            sns.kdeplot(
-                                df[x_labels[i]][df[column_name] == value],
-                                ax=self.axes[j, i],
-                                color=legend_color[num],
-                                legend=False,
-                                shade=True,
-                            )
-
-                    # Set plot labels, only set the outter plots to avoid
-                    # label overlapping
-                    if i == 0:
-                        if j == len(x_labels) - 1:
-                            # Case for bottom left corner
-                            self.axes[j, i].set_xlabel(x_labels[i], fontsize=8)
-                        else:
-                            self.axes[j, i].set_xlabel("")
-                            self.axes[j, i].set_xticklabels([]) # Turn off tick labels
-                        self.axes[j, i].set_ylabel(y_labels[j], fontsize=8)
-                    elif j == len(x_labels) - 1:
-                        self.axes[j, i].set_xlabel(x_labels[i], fontsize=8)
-                        self.axes[j, i].set_ylabel("")
-                        self.axes[j, i].set_yticklabels([]) # Turn off tick labels
-                    else:
-                        # Hide labels
-                        self.axes[j, i].set_xlabel("")
-                        self.axes[j, i].set_ylabel("")
-
-                        # Turn off tick labels
-                        self.axes[j, i].set_xticklabels([])
-                        self.axes[j, i].set_yticklabels([])
-
-            end_message = "Pair plots finished"
-            pub.sendMessage("LOG_MESSAGE", log_message=end_message)
-
-            handles, _ = self.axes[0,0].get_legend_handles_labels()
-            self.figure.legend(
-                handles,
-                labels=legend_labels,
-                title=legend_title,
-                loc='center right',
-            )
-
-            self.figure.subplots_adjust(
-                left=0.03,  # the left side of the subplots of the figure
-                bottom=0.08,  # the bottom of the subplots of the figure
-                right=0.93,  # the right side of the subplots of the figure
-                top=0.97,  # the top of the subplots of the figure
-                wspace=0.12,  # the amount of width reserved for space between subplots
-                hspace=0.12,  # the amount of height reserved for space between subplots
-            )
-
-        except ValueError as e:
-            # log Error
-            _log_message = "\nPair plots failed due to error:\n--> {}".format(e)
-            pub.sendMessage("LOG_MESSAGE", log_message=_log_message)
+        make_pair_plot(self.figure, self.df, column_name, self.available_columns)
 
         self.canvas.draw()
         self.Refresh()
